a52eb453aa69c407e2c13d86986c81eb662d37a1
Tsubasa <gumirain@gmail.com>
2020-03-31 18:00:35 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,8 +15,4 @@
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::get('hello', function () {
-    return  '<html><head><title>HELLO</title></head><body><p>こんにちは</p></body></html>';
 });
