00cfad472961567c68be2a535a08d8caf26ad04b
Ono111 <k_taro.1119@ezweb.ne.jp>
2020-04-03 02:11:05 +0900

    テスト

--- app/Http/Controllers/HelloControllerEx.php
+++ app/Http/Controllers/HelloControllerEx.php
@@ -8,43 +8,61 @@ use Illuminate\Http\Response;
 class HelloControllerEx extends Controller
 {
     public function index(Request $req, Response $res){
-       
+
 
         $html = <<<EOF
-
-         <html>
-         <head>
-         <title>HelloEx</title>
-         <style>
-         body {font-size:16pt; color:#999; }
-         h1 { font-size:120pt; text-align:right; color:#fafafa;
+        <html>
+        <head>
+        <title>HelloEx</title>
+        <style>
+        body {font-size:16pt; color:#999; }
+        h1 { font-size:120pt; text-align:right; color:#fafafa;
               margin:-50px 0px -120px 0px; 
             }
-         </style>
-         </head>
-         <body>
-           <h1>Hello</h1>
-           <h3>Request</h3>
-           <pre>{$req}</pre>
-           <h3>Response</h3>
-           <pre>{$res}</pre>
-         </body>
-         </html>
+        </style>
+        </head>
+        <body>
+          <h1>Hello</h1>           
+          <h3>Request</h3>
+          <pre>{$req}</pre>
+          <h3>Response</h3>
+          <pre>{$res}</pre>
+        </body>
+        </html>
         EOF;
 
           $res->setContent($html);
           return $res;
     }
 
-    public function index1($id='zero',Request $req){
+    public function index1($id='zero',Request $req)
+    {
         $data = ['msg'=>'これは、コントローラーから渡されたメッセージです。',
                   'id'=>$req->id];
         return view('hello.index', $data);
     }
 
-    public function index2(Request $req){
-        $data = ['msg'=>'これは、Bladeを利用したサンプルです。',
-                ];
+    public function index2(Request $req)
+    {
+        $data = ['msg'=>'これは、Bladeを利用したサンプルです。',];
         return view('hello.index', $data);
     }
+
+    public function index3(){
+        $data = ['msg'=>'',];
+        return view('hello.index', $data);
+    }
+
+    public function post(Request $req)
+    {
+       $msg = $req->msg;
+       $data = ['msg'=>$msg];
+       return view('hello.index', $data);
+    }
+
+    public function index4()
+    {
+        return view('hello.index4');
+    }
+
 }
